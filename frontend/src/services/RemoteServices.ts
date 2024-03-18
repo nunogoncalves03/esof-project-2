fe7e72772f3c3297c56310d259cbd038e4c41d4b
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2024-03-18 16:59:13 +0000

    feat(assessment): create getVolunteerAssessments remote service (#153)

--- frontend/src/services/RemoteServices.ts
+++ frontend/src/services/RemoteServices.ts
@@ -13,7 +13,8 @@ import AuthPasswordDto from '@/models/user/AuthPasswordDto';
 import Theme from '@/models/theme/Theme';
 import Enrollment from '@/models/enrollment/Enrollment';
 import Assessment from '@/models/assessment/Assessment';
-import Participation from '@/models/participation/Participation';
+import * as http from "http";
+import Participation from "@/models/participation/Participation";
 
 const httpClient = axios.create();
 httpClient.defaults.timeout = 100000;
@@ -535,6 +536,19 @@ export default class RemoteServices {
       .then((response) => {
         return response.data.map((participation: any) => {
           return new Participation(participation);
+        });
+      })
+      .catch(async (error) => {
+        throw Error(await this.errorMessage(error));
+      });
+  }
+
+  static async getVolunteerAssessments(): Promise<Assessment[]> {
+    return httpClient
+      .get('/assessments/volunteer')
+      .then((response) => {
+        return response.data.map((assessment: any) => {
+          return new Assessment(assessment);
         });
       })
       .catch(async (error) => {
